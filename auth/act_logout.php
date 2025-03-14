9d791db18b1a029964f625d3e0f80b8758ea70c5
yimemories <94780705+adzka-sfr@users.noreply.github.com>
2025-03-14 09:56:17 +0700

    perbaiki action logout

--- auth/act_logout.php
+++ auth/act_logout.php
@@ -1,15 +1,15 @@
 <?php
 $cookie_name = "duit_token";
-$cookie_path = "/duit/"; // Must match the path used when setting the cookie
-// $cookie_domain = ""; // local
-$cookie_domain = "duit.adzkasfr.com"; // hosting
+$cookie_path = "/rt_pintar/"; // Must match the path used when setting the cookie
+$cookie_domain = ""; // local
+// $cookie_domain = "duit.adzkasfr.com"; // hosting
 
 // Unset the cookie from the PHP superglobal array
 unset($_COOKIE[$cookie_name]);
 
 // Attempt to clear the cookie with various paths and domains
 setcookie($cookie_name, "", time() - 3600, $cookie_path, $cookie_domain, false, true);
-setcookie($cookie_name, "", time() - 3600, "/duit/", $cookie_domain, false, true);
+setcookie($cookie_name, "", time() - 3600, "/rt_pintar/", $cookie_domain, false, true);
 setcookie($cookie_name, "", time() - 3600, "/", $cookie_domain, false, true);
 
 // Try with and without domain
@@ -17,6 +17,6 @@ setcookie($cookie_name, "", time() - 3600, $cookie_path, "", false, true);
 setcookie($cookie_name, "", time() - 3600, "/", "", false, true);
 
 // Redirect to login page
-// header("Location: /duit/auth/main.php?page=login"); // local
-header("Location: /auth/main.php?page=login"); // hosting
+header("Location: /rt_pintar/auth/main.php?page=login"); // local
+// header("Location: /auth/main.php?page=login"); // hosting
 exit();
